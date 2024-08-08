d1756c3b1d63f1f7cf2b68cd972fed2255967d1d
m3dev4 <m3dev4@gmail.com>
2024-08-08 14:01:19 +0000

    fix bug

--- components/productList.jsx
+++ components/productList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addItem } from './CartSlice';
+import { addItem } from './cartSlice';
 import './ProductList.css';
 import CartItem from './CartItem';
 
